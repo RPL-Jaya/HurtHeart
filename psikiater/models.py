92580fcf8afa15ff17c53f516b2598a1b4461ac7
asbag2006 <marietha.asnat14@gmail.com>
2023-12-01 22:26:59 +0800

    Psikiater initial model

---
+++ psikiater/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from authentication.models import User
+
+# Create your models here.
+class Psikiater(User):
+    kualifikasi = models.BooleanField(default=False)
+    ulasanKonsultasi = models.TextField(default="")
+    jadwalKonsultasi = models.DateTimeField(auto_now_add=True)
+
+    REQUIRED_FIELDS = ['kualifikasi', 'ulasanKonsultasi', 'jadwalKonsultasi']
+
+
+class PsikiaterManager(models.Manager):
+    # List of psikiater
+    def list_psikiater(self):
+        return self.all()
+
+    # Create psikiater
+    def create_psikiater(self, data):
+        psikiater = self.create(
+            username=data['username'],
+            email=data['email'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            date_of_birth=data['date_of_birth'],
+            role=data['role'],
+            kualifikasi=data['kualifikasi'],
+            ulasanKonsultasi=data['ulasanKonsultasi'],
+            jadwalKonsultasi=data['jadwalKonsultasi'],
+        )
+        return psikiater
