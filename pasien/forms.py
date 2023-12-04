b91bdc672d4263b321451aa544c2ffc1409ca866
kayleerudaina <kayleerudaina@gmail.com>
2023-12-04 17:48:40 +0700

    add Pembayaran form

--- pasien/forms.py
+++ pasien/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Ulasan
+from .models import Ulasan, Pembayaran
 from django import forms
 
 class UlasanForm(ModelForm):
@@ -7,3 +7,7 @@ class UlasanForm(ModelForm):
         model = Ulasan
         fields = ('pasien', 'psikiater', 'komentar', 'rating', 'tanggalKonsultasi', 'namaPsikiater')
 
+class PembayaranForm(ModelForm):
+    class Meta:
+        model = Pembayaran
+        exclude = ['pasien', 'biayaPembayaran', 'statusPembayaran']
