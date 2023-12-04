0e6f62f82bd504810e2ed35d83f370e7dd4eebe2
Rafinal Haryokusumo Taloputra <rafinal.haryokusumo@ui.ac.id>
2023-12-04 17:36:15 +0700

    fix admin

--- adminhurt/urls.py
+++ adminhurt/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import payment_verification
 
-app_name = 'psikiater'
+app_name = 'adminhurt'
 urlpatterns = [
     path('payment-verification/', payment_verification, name='payment-verification'),
 ]
