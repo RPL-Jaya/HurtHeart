a558f808351379f799050efdc7bba3659e4408bc
kayleerudaina <kayleerudaina@gmail.com>
2023-12-04 19:53:35 +0700

    add Buat Pesanan

--- pasien/urls.py
+++ pasien/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import buat_ulasan, buat_ulasan_api, pesanan_konsultasi_pasien, buat_pembayaran
+from .views import buat_ulasan, buat_ulasan_api, pesanan_konsultasi, buat_pembayaran, buat_pesanan
 
 app_name = 'pasien'
 urlpatterns = [
     path('ulas/', buat_ulasan, name='ulas'),
     path('api/ulas/', buat_ulasan_api, name='ulas_api'),
-    path('pesanan/', pesanan_konsultasi_pasien, name='pesanan-list-pasien'),
-    path('pembayaran/', buat_pembayaran, name='buat-pembayaran'),
+    path('pesanan/', pesanan_konsultasi, name='pesanan-list-pasien'),
+    path('pembayaran/', buat_pembayaran, name='buat_pembayaran'),
+    path('buat-pesanan/', buat_pesanan, name='buat_pesanan')
 ]
