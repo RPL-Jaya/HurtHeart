0fb4b84f706dc503b2fee50ff11b379b3a8e6b4e
Rafinal Haryokusumo Taloputra <rafinal.haryokusumo@ui.ac.id>
2023-12-04 10:06:10 +0700

    resolve merge

--- admin/views.py
+++ admin/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
-
+from pasien.models import Pembayaran
 
 # Create your views here.
+# create view for payment verification
+def payment_verification(request):
+    # get all payment data
+    payment = Pembayaran.objects.filter().update(statusPembayaran=True)
+    # create context
+    context = {
+        "payment":payment
+    }
+    # render page
+    return render(request,"admin/payment_verification.html",context)
