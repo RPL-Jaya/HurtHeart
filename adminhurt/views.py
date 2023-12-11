9bbbadbf83125a49332086d932ddf9d08b28f623
Rafinal Haryokusumo Taloputra <rafinal.haryokusumo@ui.ac.id>
2023-12-11 20:14:17 +0700

    finishing payment verif

--- adminhurt/views.py
+++ adminhurt/views.py
@@ -8,22 +8,24 @@ from django.contrib.auth.decorators import login_required
 # create view for payment verification
 @login_required(login_url='/login/')
 def payment_verification(request,pk):
+    # get all payment data
+    payment = Pembayaran.objects.filter(id=pk)
     if request.method == "POST" and 'btn-accept' in request.POST:    
         # get all payment data
-        payment = Pembayaran.objects.filter(id=pk).update(statusPembayaran=True)
+        payment.update(statusPembayaran=True)
         # create context
         context = {
             "payment":payment
         }
     elif request.method == "POST" and 'btn-reject' in request.POST:
         # get all payment data
-        payment = Pembayaran.objects.filter(id=pk).update(statusPembayaran=False)
+        payment.update(statusPembayaran=False)
         # create context
         context = {
             "payment":payment
         }
     # render page
-    return render(request,"admin/payment_verification.html",context)
+    return render(request,"verifikasipembayaran.html",context)
 
 # create view for reading Pembayaran model
 @login_required(login_url='/login/')
@@ -32,7 +34,7 @@ def read_payment(request):
     payment = Pembayaran.objects.all()
     # create context
     context = {
-        "payment":payment,
+        "pembayaran_list":payment,
     }
     # render page
     return render(request,"read-payment.html",context)
