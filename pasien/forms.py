61c0d0638b36a7a624a059bf5dda27459d9dc417
kayleerudaina <kayleerudaina@gmail.com>
2023-12-11 16:32:43 +0700

    change pasien

--- pasien/forms.py
+++ pasien/forms.py
@@ -1,7 +1,6 @@
 from django.forms import ModelForm
 from .models import Ulasan, Pembayaran, PesananKonsultasi
 from django import forms
-from psikiater.models import JadwalKonsultasi 
 
 class UlasanForm(ModelForm):
     class Meta:
