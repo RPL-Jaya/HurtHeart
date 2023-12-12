6a32c52831cebc164600a51bc3a14a5835bd750d
NaufalFikriHakim <naufalfikri2903@gmail.com>
2023-12-13 02:56:18 +0700

    fixing bugs

--- pasien/views.py
+++ pasien/views.py
@@ -103,12 +103,13 @@ def buat_pembayaran(request, jadwal_id):
                 byte_data = base64.b64encode(file.read())
                 byte_data = byte_data.decode('utf-8')
 
-            Pembayaran.objects.create(pesanan=PesananKonsultasi.objects.get(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id)), 
+            pembayaran = Pembayaran.objects.create(pesanan=PesananKonsultasi.objects.get(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id)), 
                                       metodePembayaran=form.cleaned_data["metodePembayaran"],
                                       byte_image=byte_data)
 
-            pesanan_konsultasi = PesananKonsultasi.objects.get(jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id), pasien=request.user)
+            pesanan_konsultasi = pembayaran.pesanan
             pesanan_konsultasi.status = PesananKonsultasi.VERIFY
+            pesanan_konsultasi.save()
             return redirect("/pesanan")
         
     if not PesananKonsultasi.objects.filter(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id)).exists():
