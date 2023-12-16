d22db4bf9b99781176c1233acb5a408bcca476bf
asbag2006 <marietha.asnat14@gmail.com>
2023-12-16 13:46:13 +0800

    Fix ulas on empty

--- pasien/views.py
+++ pasien/views.py
@@ -26,14 +26,14 @@ def buat_ulasan(request):
         form = UlasanForm(request.POST)
         if form.is_valid():
             pesanan_konsultasi_pasien = PesananKonsultasi.objects.get(id=form.cleaned_data["tanggal"])
-            Ulasan.objects.create(pasien=Pasien.objects.get(user=request.user), 
+            Ulasan.objects.create(pasien=Pasien.objects.get(user=request.user),
                                   psikiater=Psikiater.objects.get(user=pesanan_konsultasi_pasien.jadwal_konsultasi.psikiater),
                                   pesanan=pesanan_konsultasi_pasien,
                                   komentar=form.cleaned_data["komentar"],
                                   rating=form.cleaned_data["rating"])
             return redirect('/buat-pesanan')
 
-    pesanan_konsultasi_pasien = PesananKonsultasi.objects.filter(pasien=request.user)
+    pesanan_konsultasi_pasien = PesananKonsultasi.objects.filter(pasien=request.user, status=PesananKonsultasi.DONE, ulasan__isnull=True)
     print(pesanan_konsultasi_pasien)
     context = {'form':form, 'pesanan_konsulatasi':pesanan_konsultasi_pasien}
     return render(request, 'ulas.html', context)
@@ -103,7 +103,7 @@ def buat_pembayaran(request, jadwal_id):
                 byte_data = base64.b64encode(file.read())
                 byte_data = byte_data.decode('utf-8')
 
-            pembayaran = Pembayaran.objects.create(pesanan=PesananKonsultasi.objects.get(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id)), 
+            pembayaran = Pembayaran.objects.create(pesanan=PesananKonsultasi.objects.get(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id)),
                                       metodePembayaran=form.cleaned_data["metodePembayaran"],
                                       byte_image=byte_data)
 
@@ -111,10 +111,10 @@ def buat_pembayaran(request, jadwal_id):
             pesanan_konsultasi.status = PesananKonsultasi.VERIFY
             pesanan_konsultasi.save()
             return redirect("/pesanan")
-        
+
     if not PesananKonsultasi.objects.filter(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id)).exists():
         PesananKonsultasi.objects.create(pasien=request.user, jadwal_konsultasi=Jadwal.objects.get(id=jadwal_id))
-    
+
     jadwal = Jadwal.objects.get(id=jadwal_id)
     context = {'form': form, "jadwal":jadwal}
     return render(request, 'pembayaran.html', context)
